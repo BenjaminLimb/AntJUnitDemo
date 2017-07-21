1a23102297ac904d05e5d88706aa10d6192dddf6
Benjamin Limb <benjaminlimb@gmail.com>
2017-07-21 16:00:16 -0600

    Initial code commit of working demo.

---
+++ src/com/benjaminlimb/antJUnitTutorial/TestMain.java
@@ -0,0 +1,12 @@
+package com.benjaminlimb.antJUnitTutorial;
+
+import junit.framework.TestCase;
+
+public class TestMain extends TestCase {
+  
+  public void testCanExecuteSimpleAssertion() {
+//    assertEquals("This String", "This String");
+    // fail();
+  }
+
+}
